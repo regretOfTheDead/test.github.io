6c57caee8019d479eb5f8ec5ba3d617a066993ed
regretOfTheDead <soufianea77@gmail.com>
2017-02-15 22:00:02 +0100

    Add files via upload

---
+++ portfolio/views/js/script.js
@@ -0,0 +1,11 @@
+var w = window,
+    d = document,
+    e = d.documentElement,
+    g = d.getElementsByTagName('body')[0],
+    x = w.innerWidth || e.clientWidth || g.clientWidth,
+    y = w.innerHeight|| e.clientHeight|| g.clientHeight;
+
+console.log(y);
+
+var header = document.getElementById("homefront");
+header.style.height = y;
